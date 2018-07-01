ba9b03adef9c4454631bad38a04c4da0a2ee3419
chenxizhan <chenxizhan1995@163.com>
2018-07-02 07:28:03 +0800

    修正用户名分页显示url 的拼写错误
    modified:   WebRoot/WEB-INF/jsp/index.jsp
    modified:   src/com/jxdedu/controller/UserController.java

--- FollowSystem-SSM/src/com/jxdedu/controller/UserController.java
+++ FollowSystem-SSM/src/com/jxdedu/controller/UserController.java
@@ -19,18 +19,18 @@ import com.jxdedu.biz.UserBiz;
 import com.jxdedu.entity.User;
 
 @Controller
-@SessionAttributes({"username", "pagingStatus"}) 
+@SessionAttributes({"username", "pagingStatus"})
 public class UserController {
-    
-    private Logger logger = Logger.getLogger(UserController.class.getName()); 
-    
+
+    private Logger logger = Logger.getLogger(UserController.class.getName());
+
     @Resource(name="userBiz")
     private UserBiz biz;
-    
+
     @RequestMapping(value="/login",method=RequestMethod.POST)
     public String login(String loginName, String loginPassword, Model model){
         logger.debug("用户登录:" + loginName + ":" + loginPassword + ":");
-        
+
         if (biz.isValid(loginName, loginPassword)){ // 登录成功
             model.addAttribute("username", loginName);
             return "index";
@@ -50,13 +50,13 @@ public class UserController {
      * @param model
      * @return
      */
-    @RequestMapping("/sysadm/paginShow")
+    @RequestMapping("/sysadm/pagingShow")
     public String pagingShow(Integer currentPage,HttpSession session, Model model){
         String psKey = "pagingStatus";
         PagingStatus ps = (PagingStatus)session.getAttribute(psKey);
-        
+
         int totalCount = biz.getTotalCount();   // 数据库中当前记录总数
-        
+
         /* 第一次进入分页界面, */
         if (ps == null){
             // 默认,当前页面 1, 分页大小 5,记录总数为现场查询的结果.
@@ -64,11 +64,11 @@ public class UserController {
             session.setAttribute(psKey, ps);
             currentPage = ps.getCurrentPage();
         }
-        
+
         /* 当前页号 */
         //  应用存在逻辑错误,或用户使用非法URL访问.
-        if (currentPage == null || currentPage < 1 
-                || currentPage > ps.getPageCount()){   
+        if (currentPage == null || currentPage < 1
+                || currentPage > ps.getPageCount()){
             logger.warn("检测到非法分页页号:" + currentPage);
             // 纠正页号.
             currentPage = (currentPage == null ? 1 : (currentPage < 1 ? 1: ps.getPageCount()) );
@@ -76,7 +76,7 @@ public class UserController {
         }
         logger.info("分页查询, 页号:" + currentPage);
         ps.setCurrentPage(currentPage);
-        
+
         /* 计算区间, 超尾区间 */
         int pageSize = ps.getPageSize();
         int lowerBound = pageSize * (currentPage - 1 ) + 1;
@@ -84,19 +84,19 @@ public class UserController {
         if (upperBound > totalCount+1){   // 为防止日后实现可能无法正确处理  upperBound 超出记录总数而导致异常
             upperBound = totalCount+1;    // 将过界的区间尾规范到正常范围
         }
-        
-        /* 获取当前分页的数据 */ 
+
+        /* 获取当前分页的数据 */
         List<User> list = biz.getByRange(lowerBound, upperBound);
-        
+
         if (list == null){
             logger.warn("分页查询结果为 null");
         }
-        
+
         /* 向视图层传递数据 */
         model.addAttribute("list",list);
         return "user/paging-show";
     }
-    
+
     /**
      * 查询指定用户是否存在;
      * @param userName
